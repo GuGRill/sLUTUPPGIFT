56a67a9f13cefb379dbf0b8d70bb5106854b36d1
GuGRill <102228320+GuGRill@users.noreply.github.com>
2022-05-24 16:05:47 +0200

    Add files via upload

--- Js.js
+++ Js.js
@@ -3,12 +3,20 @@ function myFunction() {    // Get the checkbox
   
     // If the checkbox is checked, display the output text
     if (checkBox.checked == true){
-        document.body.style.backgroundColor="black"; 
-        document.body.style.color="white";
+        document.body.style.backgroundColor="rgb(39, 39, 39)"; 
+        document.body.style.color="white";        
+
     } 
     else
     {
         document.body.style.backgroundColor="white";
         document.body.style.color="black";
     }
+    save();
 }
+function save() {
+    var checkbox = document.getElementById("myCheck");
+    localStorage.setItem("myCheck", checkbox.checked);
+}
+var checked = JSON.parse(localStorage.getItem("myCheck"));
+document.getElementById("myCheck").checked = checked;
